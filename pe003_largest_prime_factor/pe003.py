f40949bf5f5a56c91dcaa7e3f872d75cb0c8fe9c
Zdeněk Němec <zdenek.nemec@artin.cz>
2017-11-15 14:28:17 +0100

    Corrected descriptions in PE002 and PE003

--- pe003_largest_prime_factor/pe003.py
+++ pe003_largest_prime_factor/pe003.py
@@ -17,7 +17,7 @@ Examples:
     ``pe003.py``
 
 Description:
-    Solution for Project Euler Problem 3 (https://projecteuler.net/problem=3).
+    Solution for Project Euler problem 3 (https://projecteuler.net/problem=3).
 
     The prime factors of 13195 are 5, 7, 13 and 29.
 
