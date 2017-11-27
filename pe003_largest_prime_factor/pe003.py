d906262fb3de774963f1e7837b50e7c7d40b099f
Zdeněk Němec <zdenek.nemec@artin.cz>
2017-11-27 11:00:46 +0100

    PE003: Python 3

--- pe003_largest_prime_factor/pe003.py
+++ pe003_largest_prime_factor/pe003.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 PE003: Largest prime factor
@@ -8,7 +8,7 @@ Name: pe003.py
 
 Author: Zdenek Nemec <zdenek.nemec@artin.cz>
 
-Version: 2.0 (2017-11-15)
+Version: 3.0 (2017-11-27)
 
 Synopsis:
     ``pe003.py``
@@ -25,47 +25,50 @@ Description:
 """
 
 
+import math
+
+
 NUMBER = 600851475143
 
 
-# Solution: Factors of factors ################################################
-
 class Factors(object):
-    def __init__(self):
-        pass
-
     @staticmethod
     def get_factors(number):
         factors = []
         possible_factor = 2
-        cap = number / 2
+        cap = math.sqrt(number)
         while possible_factor <= cap:
             if number % possible_factor == 0:
                 factors.append(possible_factor)
                 factors.append(number / possible_factor)
             possible_factor += 1
-            cap = number / possible_factor
         factors.sort()
         return factors
 
-    def get_prime_factors(self, number):
-        factors = self.get_factors(number)
-        prime_factors = []
-        for possible_prime in factors:
-            if not(self.get_factors(possible_prime)):
-                prime_factors.append(possible_prime)
-        prime_factors.sort()
-        return prime_factors
 
-    def get_largest_prime_factor(self, number):
-        prime_factors = self.get_prime_factors(number)
-        return prime_factors[-1]
+class Primes(object):
+    @staticmethod
+    def is_prime(number):
+        prime_flag = False
+        if not Factors.get_factors(number):
+            prime_flag = True
+        return prime_flag
 
 
-# Main ########################################################################
+class Solution(object):
+    @staticmethod
+    def solve(number):
+        factors = Factors.get_factors(number)
+        primes = []
+        for selected_factor in factors:
+            if Primes.is_prime(selected_factor):
+                primes.append(selected_factor)
+        primes.sort()
+        return primes[-1]
+
 
 def main():
-    print Factors().get_largest_prime_factor(NUMBER)
+    print(Solution().solve(NUMBER))
 
 
 if __name__ == "__main__":
--- pe003_largest_prime_factor/test_pe003.py
+++ pe003_largest_prime_factor/test_pe003.py
@@ -1,42 +1,21 @@
 import unittest
 
 from parameterized import parameterized
-from pe003 import Factors
+from pe003_largest_prime_factor.pe003 import Solution
 
 
-class FactorsTest(unittest.TestCase):
-    @parameterized.expand([
-        [10, [2, 5]],
-        [24, [2, 3, 4, 6, 8, 12]]
-    ])
-    def test_get_factors(self, number, expected_result):
-        factors = Factors().get_factors(number)
-        self.assertEqual(
-            factors,
-            expected_result,
-            ("Result should be", expected_result, "but is", factors))
-
-    @parameterized.expand([
-        [13195, [5, 7, 13, 29]]
-    ])
-    def test_get_prime_factors(self, number, expected_result):
-        prime_factors = Factors().get_prime_factors(number)
-        self.assertEqual(
-            prime_factors,
-            expected_result,
-            ("Result should be", expected_result, "but is", prime_factors))
-
+class SolutionTest(unittest.TestCase):
     @parameterized.expand([
         [13195, 29],
         [600851475143, 6857]
     ])
-    def test_get_largest_prime_factor(self, number, expected_result):
-        largest_prime_factor = Factors().get_largest_prime_factor(number)
+    def test_solve(self, number, expected_prime_factor):
+        largest_prime_factor = Solution().solve(number)
         self.assertEqual(
             largest_prime_factor,
-            expected_result,
-            "Result should be %d but is %d."
-            % (expected_result, largest_prime_factor))
+            expected_prime_factor,
+            "The result should be %d but is %d."
+            % (expected_prime_factor, largest_prime_factor))
 
 
 if __name__ == "__main__":
