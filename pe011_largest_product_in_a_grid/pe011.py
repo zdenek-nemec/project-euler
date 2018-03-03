2e47cb61610ce1cfdf2e7dff04926936b454d4bb
zdenek-nemec <jj3Mk7ew0oFCnwAx>
2018-03-03 19:49:37 +0100

    New code style and unit tests for PE-011

--- pe011_largest_product_in_a_grid/pe011.py
+++ pe011_largest_product_in_a_grid/pe011.py
@@ -1,150 +1,121 @@
 #!/usr/bin/env python3
 
 """
-PE011: Largest product in a grid
---------------------------------
+PE-011: Largest product in a grid
+---------------------------------
 
-Name: pe011.py
+Solution for Project Euler Problem 11 (https://projecteuler.net/problem=11).
 
-Author: Zdenek Nemec <zdenek.nemec@artin.cz>
+In the :math:`20 \\times 20` grid below, four numbers along a diagonal line have
+been marked in red.
 
-Version: 4.0 (2017-11-27)
+| 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+| 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+| 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+| 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+| 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+| 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+| 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+| 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+| 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+| 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+| 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+| 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+| 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+| 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+| 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+| 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+| 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+| 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+| 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+| 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
 
-Synopsis:
-    ``pe011.py``
+The product of these numbers is
+:math:`26 \\times 63 \\times 78 \\times 14 = 1788696`.
 
-Examples:
-    ``pe011.py``
-
-Description:
-    Solution for Project Euler Problem 11
-    (https://projecteuler.net/problem=11).
-
-    In the :math:`20 \\times 20` grid below, four numbers along a diagonal
-    line have been marked in red.
-
-    | 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
-    | 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
-    | 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
-    | 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
-    | 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
-    | 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
-    | 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
-    | 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
-    | 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
-    | 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
-    | 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
-    | 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
-    | 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
-    | 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
-    | 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
-    | 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
-    | 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
-    | 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
-    | 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
-    | 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
-
-    The product of these numbers is
-    :math:`26 \\times 63 \\times 78 \\times 14 = 1788696`.
-
-    What is the greatest product of four adjacent numbers in the same
-    direction (up, down, left, right, or diagonally) in the
-    :math:`20 \\times 20` grid?
+What is the greatest product of four adjacent numbers in the same direction (up,
+down, left, right, or diagonally) in the :math:`20 \\times 20` grid?
 """
 
 
 import csv
-import sys
 
 
+INPUT_FILENAME = 'numbers.csv'
 SEQUENCE = 4
 
 
-# Solution: Brute Force #######################################################
+class Solution(object):
+    @staticmethod
+    def solve(input_filename, sequence):
+        numbers = []
+        with open(input_filename, 'r') as csv_file:
+            reader = csv.reader(csv_file, delimiter=' ')
+            for row in reader:
+                numbers.append(list(map(int, row)))
+        assert len(numbers) == 20, 'The matrix is expected to have 20 rows.'
+        for row in numbers:
+            assert len(row) == 20, 'Each row of the matrix is expected to ' \
+                                   'have 20 values (columns).'
 
-def solve_brute_force(numbers, sequence):
-    """
-    Go through all values per row, then per column, then per diagonal,
-    calculate product and select the greatest.
-    """
-    rows = len(numbers)
-    cols = len(numbers[0])
-    for row in numbers:
-        if (len(row) != cols):
-            return -1
+        rows = len(numbers)
+        columns = len(numbers[0])
+        max_product = 0
 
-    max_prod = 0
-
-    # Scan rows
-    for y in range(0, rows):
-        for x in range(0, cols):
-            prod = 1
-            for i in range(0, sequence):
-                if ((x+i) >= cols):
-                    prod = 0
-                    break
-                prod *= numbers[y][x+i]
-            if (prod > max_prod):
-                max_prod = prod
-
-    # Scan columns
-    for x in range(0, cols):
+        # Scan rows
         for y in range(0, rows):
-            prod = 1
-            for i in range(0, sequence):
-                if ((y+i) >= rows):
-                    prod = 0
-                    break
-                prod *= numbers[y+i][x]
-            if (prod > max_prod):
-                max_prod = prod
+            for x in range(0, columns):
+                product = 1
+                for i in range(0, sequence):
+                    if x + i >= columns:
+                        product = 0
+                        break
+                    product *= numbers[y][x+i]
+                if product > max_product:
+                    max_product = product
 
-    # Scan diagonal from top-left to bottom-right
-    for y in range(0, rows):
-        for x in range(0, cols):
-            prod = 1
-            for i in range(0, sequence):
-                if (((x+i) >= cols) or ((y+i) >= rows)):
-                    prod = 0
-                    break
-                prod *= numbers[y+i][x+i]
-            if (prod > max_prod):
-                max_prod = prod
+        # Scan columns
+        for x in range(0, columns):
+            for y in range(0, rows):
+                product = 1
+                for i in range(0, sequence):
+                    if y + i >= rows:
+                        product = 0
+                        break
+                    product *= numbers[y+i][x]
+                if product > max_product:
+                    max_product = product
 
-    # Scan diagonal from top-right to bottom-left
-    for y in range(0, rows):
-        for x in range(0, cols):
-            prod = 1
-            for i in range(0, sequence):
-                if (((x-i) < 0) or ((y+i) >= rows)):
-                    prod = 0
-                    break
-                prod *= numbers[y+i][x-i]
-            if (prod > max_prod):
-                max_prod = prod
+        # Scan diagonal from top-left to bottom-right
+        for y in range(0, rows):
+            for x in range(0, columns):
+                product = 1
+                for i in range(0, sequence):
+                    if x + i >= columns or y + i >= rows:
+                        product = 0
+                        break
+                    product *= numbers[y+i][x+i]
+                if product > max_product:
+                    max_product = product
 
-    return max_prod
+        # Scan diagonal from top-right to bottom-left
+        for y in range(0, rows):
+            for x in range(0, columns):
+                product = 1
+                for i in range(0, sequence):
+                    if x - i < 0 or y + i >= rows:
+                        product = 0
+                        break
+                    product *= numbers[y+i][x-i]
+                if product > max_product:
+                    max_product = product
 
+        return max_product
 
-# Main ########################################################################
 
 def main():
-    # Load the numbers from numbers.txt file
-    numbers = []
-    with open("numbers.txt", "r") as csvfile:
-        reader = csv.reader(csvfile, delimiter = ' ')
-        for row in reader:
-            numbers.append(list(map(int, row)))
-    assert (len(numbers) == 20), "The matrix is expected to have 20 rows."
-    for row in numbers:
-        assert (len(row) == 20), "Each row of the matrix is expected to have 20 values (columns)."
-
-    result = solve_brute_force(numbers, SEQUENCE)
-    print("Solution: Brute Force")
-    print("\tThe greatest product of", SEQUENCE, "adjacent numbers is", result)
-
-    return 0
+    print(Solution().solve(INPUT_FILENAME, SEQUENCE))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
