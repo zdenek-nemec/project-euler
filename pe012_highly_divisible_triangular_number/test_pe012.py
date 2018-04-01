2762dbb62ab5fcd89bbd3ea0ffda4a18cfbaf022
zdenek-nemec <zdenek.nemec@artin.cz>
2018-04-01 23:07:09 +0200

    Removed static methods in PE-012 solution

--- pe012_highly_divisible_triangular_number/test_pe012.py
+++ pe012_highly_divisible_triangular_number/test_pe012.py
@@ -7,15 +7,14 @@ from pe012_highly_divisible_triangular_number.pe012 import Solution
 class SolutionTest(unittest.TestCase):
     @parameterized.expand([
         [5, 28],
-        [500, 76576500]
-    ])
+        [500, 76576500]])
     def test_solve(self, divisors_limit, expected_triangle_number):
-        calculated__triangle_number = Solution().solve(divisors_limit)
+        returned_triangle_number = Solution(divisors_limit).solve()
         self.assertEqual(
-            calculated__triangle_number,
+            returned_triangle_number,
             expected_triangle_number,
             'The result should be %d but is %d.'
-            % (expected_triangle_number, calculated__triangle_number))
+            % (expected_triangle_number, returned_triangle_number))
 
 
 if __name__ == '__main__':
