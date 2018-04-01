ee5b905262d87cd7979a3638b788241d7d99f1bd
zdenek-nemec <zdenek.nemec@artin.cz>
2018-04-01 17:07:16 +0200

    Removed static methods in PE-007 solution

--- pe007_10001st_prime/pe007.py
+++ pe007_10001st_prime/pe007.py
@@ -9,73 +9,54 @@ Solution for Project Euler Problem 7 (https://projecteuler.net/problem=7).
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
 the 6th prime is 13.
 
-What is the 10001st prime number?
+What is the 10 001st prime number?
 """
+
+
+import math
 
 
 POSITION = 10001
 
 
-class Solution(object):
-    @staticmethod
-    def check_prime(number):
-        i = 2
-        top = number / 2
-        while i <= top:
-            if number % i == 0:
-                return 0
-            i += 1
-        return 1
+class Primes(object):
+    def __init__(self):
+        self._primes = []
 
-    @staticmethod
-    def check_prime_divisor(number, primes):
-        for i in primes:
-            if number % i == 0:
+    def add_prime(self, number):
+        self._primes.append(number)
+
+    def check_prime(self, number):
+        for prime in self._primes:
+            if number % prime == 0:
                 return 0
-            if i > number / 2:
+            if prime > math.sqrt(number):
                 break
         return 1
 
-    @staticmethod
-    def solve(position):
-        return Solution().solve_prime_divisor(position)
+    def get_length(self):
+        return len(self._primes)
 
-    @staticmethod
-    def solve_brute_force(position):
-        """
-        After 2 go through all odd numbers and check for primes. If the number
-        is a prime, put it into a list and scan the specific position. This is
-        effective only for primes on positions < 1000.
-        """
-        primes = [2, 3]
-        number = 3
+
+class Solution(object):
+    def __init__(self, position):
+        self._position = position
+
+    def solve(self):
+        number = 1
+        primes = Primes()
+        primes.add_prime(2)
         while True:
             number += 2
-            if Solution().check_prime(number):
-                primes.append(number)
-                if len(primes) == position:
-                    break
-        return number
-
-    @staticmethod
-    def solve_prime_divisor(position):
-        """
-        Every number that is not a prime should be divisible by a prime. Go
-        through all odd numbers after 2 and check their divisibility by primes.
-        """
-        primes = [2, 3]
-        number = 3
-        while True:
-            number += 2
-            if Solution().check_prime_divisor(number, primes):
-                primes.append(number)
-                if len(primes) == position:
+            if primes.check_prime(number):
+                primes.add_prime(number)
+                if primes.get_length() == self._position:
                     break
         return number
 
 
 def main():
-    print(Solution().solve(POSITION))
+    print(Solution(POSITION).solve())
 
 
 if __name__ == '__main__':
--- pe007_10001st_prime/test_pe007.py
+++ pe007_10001st_prime/test_pe007.py
@@ -7,15 +7,14 @@ from pe007_10001st_prime.pe007 import Solution
 class SolutionTest(unittest.TestCase):
     @parameterized.expand([
         [6, 13],
-        [10001, 104743]
-    ])
+        [10001, 104743]])
     def test_solve(self, position, expected_prime):
-        prime = Solution().solve(position)
+        returned_prime = Solution(position).solve()
         self.assertEqual(
-            prime,
+            returned_prime,
             expected_prime,
             'The result should be %d but is %d.'
-            % (expected_prime, prime))
+            % (expected_prime, returned_prime))
 
 
 if __name__ == '__main__':
