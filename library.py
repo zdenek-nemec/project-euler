7e37e1605b66531cf7252cca7b0bd2f3559e3ccc
Zdeněk Němec <zdenek.nemec@artin.cz>
2018-06-29 09:29:46 +0200

    Library: Unit tests for Fibonacci class

--- library.py
+++ library.py
@@ -2,8 +2,10 @@ import math
 
 
 class Divisors(object):
-    @staticmethod
-    def get_divisors(number):
+    def __init__(self):
+        self._divisors = {}
+
+    def _calculate_divisors(self, number):
         divisors = [1]
         for divisor in range(2, int(math.sqrt(number)) + 1):
             if number % divisor == 0:
@@ -12,16 +14,26 @@ class Divisors(object):
                     divisors.append(number // divisor)
         return sorted(divisors)
 
+    def _save_divisors(self, number, divisors):
+        self._divisors[number] = divisors
+
+    def get_divisors(self, number):
+        if number not in self._divisors:
+            divisors = self._calculate_divisors(number)
+            self._save_divisors(number, divisors)
+        return self._divisors[number]
+
+
 
 class Fibonacci(object):
-    def __init__(self, start):
-        self._series = start
+    def __init__(self, start=(1, 1)):
+        self._series = list(start)
 
     def append_next(self):
-        next_number = self._series[-2] + self._series[-1]
+        next_number = self._series[-1] + self._series[-2]
         self._series.append(next_number)
 
-    def get_all(self):
+    def get_series(self):
         return self._series
 
     def get_last(self):
@@ -31,9 +43,5 @@ class Fibonacci(object):
         return len(self._series)
 
 
-def main():
-    print('Hello World!')
-
-
 if __name__ == '__main__':
-    main()
+    pass
